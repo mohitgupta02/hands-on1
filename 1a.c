4bba0a1b13221c0b670f1c4c5a6b3d3f0a41c38b
mohitgupta02 <mohitvg786@gmail.com>
2024-08-22 08:44:41 +0530

    link created

--- 1a.c
+++ 1a.c
@@ -1,5 +1,7 @@
 //Gupta Mohit
 //MT2024049
+// Create the following types of a files using (i) shell command (ii) system call
+// a. soft link (symlink system call
 #include<stdio.h>
 #include<unistd.h>
 
