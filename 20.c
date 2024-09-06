059274e4ea3b64e1ee9ed044ef2bc082d2fdca71
mohitgupta02 <92048287+mohitgupta02@users.noreply.github.com>
2024-09-06 09:08:26 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -3,18 +3,28 @@ Name : Mohit Gupta
 Roll No. : MT2024049
 Find out the priority of your running program. Modify the priority with nice command.
 */
-#include<stdio.h>
-#include<sys/time.h>
-#include<unistd.h>
-#include<sys/resource.h>
-int main()
-{
-	int i;
-	scanf("%d", &i);
-	printf("nice value = %d\n", getpriority(PRIO_PROCESS, i));
-	printf("priority = %d\n", (20+getpriority(PRIO_PROCESS, i)));
-	return 0;
+
+
+#include <stdio.h>       // For printf()
+#include <unistd.h>      // For getpid() and nice()
+#include <sys/resource.h>// For getpriority() and PRIO_PROCESS
+
+int main(int argc, char *argv[]) {
+  
+  int pid = getpid();
+	int niceVal = getpriority(PRIO_PROCESS, 0);
+	
+	printf("Priority of process with PID %d is %d\n", pid, niceVal);
+	
+	nice(10); // updating the priority(nice value) to 10 of the current process 
+	niceVal = getpriority(PRIO_PROCESS, 0); //first argument : priority of the process is being queried , second argument : 0 : means we are refering to the current process
+
+	
+	printf("Modified priority of process %d\n", niceVal);
+
+  return 0;
 }
+
 
 /*
 3
